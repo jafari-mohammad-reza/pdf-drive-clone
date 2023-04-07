1387de277a7c62b490ef9695904e88449346fd14
DaYeezus <mohammadrezajafari.dev@gmail.com>
2023-04-07 23:02:09 +0330

    Add application modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './modules/auth/auth.module';
+import { UploadModule } from './modules/upload/upload.module';
+import { DownloadModule } from './modules/download/download.module';
+import { SearchModule } from './modules/search/search.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UploadModule, DownloadModule, SearchModule],
   controllers: [],
   providers: [],
 })
