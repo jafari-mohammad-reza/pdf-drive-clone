8d65f168576d8d7d4b37aad71ce9dbcfc16cde12
DaYeezus <mohammadrezajafari.dev@gmail.com>
2023-04-11 20:12:08 +0330

    Done tag module.

--- src/modules/tag/tag.controller.ts
+++ src/modules/tag/tag.controller.ts
@@ -1,6 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { TagService } from './tag.service';
 
 @Controller('tag')
 export class TagController {
+  constructor(private readonly tagService:TagService){}
+ @Post()
+  async createTag(@Body("title") title:string) {
+    return await this.tagService.createTag(title)
+  }
 
+ @Get()
+async getBooksByTag(@Param("title") title:string) {
+    return await this.tagService.getBooksByTag(title)
+  }
 }
--- src/modules/tag/tag.service.ts
+++ src/modules/tag/tag.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Book } from 'src/share/schemas/book.schema';
+import { Tag } from 'src/share/schemas/tag.schema';
 
 @Injectable()
-export class TagService {}
+export class TagService {
+  constructor(@InjectModel(Tag.name) private readonly tagModel :Model<Tag>){}
+  async createTag(title:string):Promise<Tag>{
+    return await this.tagModel.create({title})
+  }
+  async getBooksByTag(tagTitle:string):Promise<Book[]>{
+    return this.tagModel.findOne({title:tagTitle}).populate("books")
+  }
+
+}
